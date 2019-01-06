2555f95755d887a191c19911ad49ac0b0deb4edf
velet5 <velet5@ya.ru>
2019-01-06 19:52:18 +0300

    [experiment] one more time

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,4 @@
 cd ~/ufag
 UFAG_JAR=$(ls dist | sort | tail -n1) # xxx: remove old dists
 pkill java # xxx: replace with more appropriate way to kill current instance
-java -jar dist/$UFAG_JAR &
+java -jar -Dconfig.file=$HOME/ufag/application.conf dist/$UFAG_JAR &
