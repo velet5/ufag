6ea86937ca872ffe4771176ca17180c06cf8ffea
velet5 <velet5@ya.ru>
2019-01-07 13:58:04 +0300

    [experiment] fixing deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 
 cd ~/ufag
-UFAG_JAR=$(ls dist | sort | tail -n1) # xxx: remove old dists
-pkill java # xxx: replace with more appropriate way to kill current instance
-java -jar -Dconfig.file=$HOME/ufag/application.conf dist/$UFAG_JAR &
+
+UFAG_PID_FILE=ufag.pid
+UFAG_JAR=$(ls dist | sort | tail -n1)
+UFAG_OLD_PID=$(cat ${UFAG_PID_FILE})
+
+# SEND kill signal to current bot
+kill ${UFAG_OLD_PID}
+
+# WAIT for process to exit
+tail --pid=${UFAG_OLD_PID} -f /dev/null
+
+# START new bot
+java -jar -Dconfig.file=${HOME}/ufag/application.conf dist/${UFAG_JAR} &
+
+# SAVE pid of current launching
+echo -n $! > ${UFAG_PID_FILE}
+
+# fixme: remove old dists
