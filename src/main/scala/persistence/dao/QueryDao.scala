e7edcc79a1970e9fd314969bcdae0c9789f50614
velet5 <velet5@ya.ru>
2019-01-12 19:03:52 +0300

    [fix] #13 move everything to slick
    forgot about 30 days criteria

--- src/main/scala/persistence/dao/QueryDao.scala
+++ src/main/scala/persistence/dao/QueryDao.scala
@@ -1,5 +1,7 @@
 package persistence.dao
 
+import java.time.ZonedDateTime
+
 import persistence.Tables
 import persistence.model.Query
 import slick.dbio.Effect.{Read, Write}
@@ -10,15 +12,21 @@ import scala.concurrent.ExecutionContext
 class QueryDao()
               (implicit ec: ExecutionContext) {
 
+  import persistence.model.zonedDateTimeMapper
+
   def save(query: Query): DBIOAction[Int, NoStream, Write] =
     Tables.queries += query
 
-  def find(chatId: Long, text: String): DBIOAction[Option[Query], NoStream, Read] =
+  def find(chatId: Long, text: String): DBIOAction[Option[Query], NoStream, Read] = {
+    val thirtyDaysAgo = ZonedDateTime.now().minusDays(30)
+
     Tables.queries
       .filter(_.chatId === chatId.bind)
       .filter(_.text === text.bind)
+      .filter(_.time > thirtyDaysAgo.bind)
       .result
       .headOption
+  }
 
   def count(): DBIOAction[Int, NoStream, Read] =
     Tables.queries
