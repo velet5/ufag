96241c05d91ade6061cde7fc809f12fd45aa456e
velet5 <velet5@ya.ru>
2019-01-16 21:39:55 +0300

    [skip travis] trying get working docker deploy

--- scripts/docker-deploy.sh
+++ scripts/docker-deploy.sh
@@ -1,25 +1,24 @@
 #!/usr/bin/env bash
 
-# TODO: double-check all this
-
-UFAG_IMAGE_TAR=ufag-latest.tar
-
 # ENTER the app directory
 cd ~/ufag
+
 
 # CREATE needed directories
 mkdir -p images
 
-# BACKUP image to be able to restore it later
-cp ${UFAG_IMAGE_TAR} images/ufag-$(date +%Y-%m-%d-%H-%M-%S).tar
-
-# REMOVE old images
 cd images
+
+UFAG_IMAGE_TAR=$(ls | tail -n1)
+UFAG_TAG=$(sed -e 's/^ufage//' -e 's/\.tar$//' <<<"${UFAG_IMAGE_TAR}")
+# REMOVE old images
 rm $(ls | head -n -5)
 cd ..
 
-# LOAD image
-sudo docker load -i ${UFAG_IMAGE_TAR}
+UFAG_CONTAINER_ID=$(cat ufag.container-id)
 
-sudo docker stop $(sudo docker ps -q  --filter ancestor=velet5/ufag:latest)
-sudo docker run -d -v ~/application.conf:application.conf -v ~/logs:/logs -p8080:8080 velet5/ufag-latest
+# LOAD image
+sudo docker load -i images/${UFAG_IMAGE_TAR}
+
+sudo docker stop ${UFAG_CONTAINER_ID}
+sudo docker run -d --network=ufag-network -v $HOME/ufag/application.conf:application.conf -v $HOME/ufag/logs:/logs -p8080:8080 velet5/ufag:${UFAG_TAG} > ufag.container-id
