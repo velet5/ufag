55a879862702f328cbebb9bb0bcf1642d41b6a97
velet5 <velet5@ya.ru>
2019-01-19 14:09:19 +0300

    #24 Fix docker deployment

--- scripts/docker-restart.sh
+++ scripts/docker-restart.sh
@@ -9,25 +9,27 @@ UFAG_IMAGE_TAR=$(ls | tail -n1)
 UFAG_TAG=$(sed -e 's/^ufag-//' -e 's/\.tar$//' <<<"${UFAG_IMAGE_TAR}")
 
 # REMOVE old images
-rm $(ls | head -n -5)
+rm $(ls | head -n -1)
 cd ..
 
 # LOAD image
 docker load -i images/${UFAG_IMAGE_TAR}
 
 # STOP current container
-docker ps --filter 'ancestor=velet5/ufag' -q | xargs -r docker stop
+docker ps  | grep 'velet5/ufag' | awk "{ print $1 }" | xargs -r docker stop
+
+# RUN new container
 docker run \
     --rm \
-    -d \
+    --detach \
     --network=ufag-network \
-    -v $HOME/ufag/application.conf:/application.conf \
-    -v $HOME/ufag/logs:/logs \
-    -p8080:8080 \
+    --volume $HOME/ufag/application.conf:/application.conf \
+    --volume $HOME/ufag/logs:/logs \
+    --publish 8080:8080 \
     velet5/ufag:${UFAG_TAG}
 
 # REMOVE old images
-docker images --filter "before=velet5/ufag:${UFAG_TAG}" -q | \
-    sort | \
-    head -n -4 | \
-    xargs -r docker rmi
+docker images --filter "before=velet5/ufag:${UFAG_TAG}" | \
+    grep 'velet5/ufag' | \
+    awk "{ print $1 }" | \
+    xargs -r docker rmi -f
