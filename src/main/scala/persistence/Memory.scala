6264dd46d734fc5916e83a52b4f56f3f75dc6892
velet5 <velet5@ya.ru>
2019-02-24 21:38:17 +0300

    Small fixes

--- src/main/scala/persistence/Memory.scala
+++ src/main/scala/persistence/Memory.scala
@@ -6,23 +6,29 @@ import java.time.temporal.ChronoUnit
 object Memory {
 
   def fag(when: ZonedDateTime): String = {
-    "Похоже что ты пидор, потому что я уже отвечал на этот запрос " + timeSince(when) + " назад"
+    val timeStr = timeSince(when).fold("только что")(_ + " назад")
+
+    s"Похоже, что ты не выучил слово, потому что я уже отвечал на этот запрос $timeStr"
   }
 
   // private
 
   private case class PluralForms(`1`: String, `2-4`: String, `5-0`: String) {
-    // todo: correctly handle number 12
-    def format(number: Long): String = {
-      val form =
-        number.toString.last match {
-          case '1' => `1`
-          case '2' | '3' | '4' => `2-4`
-          case _ => `5-0`
-        }
 
-      s"$number $form"
+    def format(number: Long): String = {
+      val reminder = number % 10
+
+      if (number >= 5 && number <= 21) {
+        `5-0`
+      } else if (reminder >=2 && reminder <= 4) {
+        `2-4`
+      } else if (reminder == 1) {
+        `1`
+      } else {
+        `5-0`
+      }
     }
+
   }
 
   private val day = PluralForms("день", "дня", "дней")
@@ -30,19 +36,22 @@ object Memory {
   private val minute = PluralForms("минуту", "минуты", "минут")
   private val second = PluralForms("секунду", "секунды", "секунд")
 
-  private def timeSince(time: ZonedDateTime): String = {
+  private def timeSince(time: ZonedDateTime): Option[String] = {
     val now = ZonedDateTime.now()
     val since = Seq(
       (ChronoUnit.DAYS.between(time, now), day),
       (ChronoUnit.HOURS.between(time, now) % 24, hour),
       (ChronoUnit.MINUTES.between(time, now) % 60, minute),
-      (ChronoUnit.SECONDS.between(time, now) % 70, second))
+      (ChronoUnit.SECONDS.between(time, now) % 60, second))
 
-    since
-      .filter {case (number, _) => number > 0 }
-      .take(2)
-      .map {case (number, formatter) => formatter.format(number)}
-      .mkString(" ")
+    val str =
+      since
+        .filter {case (number, _) => number > 0 }
+        .take(2)
+        .map {case (number, formatter) => formatter.format(number)}
+        .mkString(" ")
+
+    Option(str).filter(_.nonEmpty)
   }
 
 }
